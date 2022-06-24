55800f4e99d43a6633e56d3e620c4d93ea080530
Luiz Henrique Santos <luiz.santos85@gmail.com>
2022-06-24 15:57:10 -0300

    criando a estrutura

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import LogoImage from './assets/devmemory_logo.png';
+import * as C from './App.styles';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   return (
+      <C.Container>
+         <C.Info>
+            <C.LogoLink href="">
+               <img src={LogoImage} alt="Logomarca" width={200} />
+            </C.LogoLink>
+
+            <C.InfoArea>...</C.InfoArea>
+
+            <button>Reiniciar</button>
+         </C.Info>
+
+         <C.GridArea>***</C.GridArea>
+      </C.Container>
+   );
 }
 
 export default App;
