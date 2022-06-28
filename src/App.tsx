8074f7c2823cf03308ef9c0df3f6bd9a156f2527
Luiz Henrique Santos <luiz.santos85@gmail.com>
2022-06-28 20:40:43 -0300

    implementacao dos dados, implementando o griditem

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,54 @@
 import * as C from './App.styles';
-import { InfoItem } from './components/InfoItem'
-import {ButtonItem} from './components/ButtonItem'
+import { useEffect, useState } from 'react';
+import { InfoItem } from './components/InfoItem';
+import { ButtonItem } from './components/ButtonItem';
 import LogoImage from './assets/devmemory_logo.png';
 import Restart from './svgs/restart.svg';
+import { GridItemType } from './types/GridItemType';
+import { items } from './data/items';
+import { GridItem } from './components/GridItem';
 
 function App() {
+   const [playing, setPlaying] = useState<boolean>(false);
+   const [timeElapsed, setTimeElapsed] = useState<number>(0);
+   const [moveCount, setMoveCount] = useState<number>(0);
+   const [shownCount, setShowncount] = useState<number>(0);
+   const [gridItems, setGridItems] = useState<GridItemType[]>([]);
 
-   const ResetAndCreateGrid = () =>{}
+   useEffect(() => ResetAndCreateGrid(), []);
+
+   const ResetAndCreateGrid = () => {
+      //passo 1 - resetar o jogo
+      setTimeElapsed(0);
+      setMoveCount(0);
+      setShowncount(0);
+
+      //passo 2 - criar o grid e começar o jogo
+      //cria grid vazio
+      let tmpGrid: GridItemType[] = [];
+      for (let i = 0; i < items.length * 2; i++) {
+         tmpGrid.push({
+            item: null,
+            shown: false,
+            permanentShown: false,
+         });
+      }
+
+      //preencher o grid
+      for (let w = 0; w < 2; w++) {
+         for (let i = 0; i < items.length; i++) {
+            let pos = -1;
+            while (pos < 0 || tmpGrid[pos].item !== null) {
+               pos = Math.floor(Math.random() * (items.length * 2));
+            }
+            tmpGrid[pos].item = i;
+         }
+      }
+
+      setGridItems(tmpGrid);
+
+      setPlaying(true);
+   };
 
    return (
       <C.Container>
@@ -16,15 +58,24 @@ function App() {
             </C.LogoLink>
 
             <C.InfoArea>
-               <InfoItem label='Tempo' value="00:00"/>
-               <InfoItem label='Movimentos' value="0"/>
+               <InfoItem label="Tempo" value="00:00" />
+               <InfoItem label="Movimentos" value="0" />
             </C.InfoArea>
 
-            
-            <ButtonItem label="Reiniciar" icon={Restart} onClick={ResetAndCreateGrid}/>
+            <ButtonItem
+               label="Reiniciar"
+               icon={Restart}
+               onClick={ResetAndCreateGrid}
+            />
          </C.Info>
 
-         <C.GridArea>***</C.GridArea>
+         <C.GridArea>
+            <C.Grid>
+               {gridItems.map((item,index) => (
+                  <GridItem />
+               ))}
+            </C.Grid>
+         </C.GridArea>
       </C.Container>
    );
 }
---
+++ src/data/items.ts
@@ -0,0 +1,17 @@
+import android from '../svgs/android.svg';
+import caminhao from '../svgs/caminhao.svg';
+import disney from '../svgs/disney.svg';
+import estrela from '../svgs/estrela.svg';
+import b7web from '../svgs/b7.svg';
+import gasolina from '../svgs/gasolina.svg';
+import moto from '../svgs/moto.svg';
+
+export const items = [
+   { name: 'android', icon: android },
+   { name: 'caminhao', icon: caminhao },
+   { name: 'disney', icon: disney },
+   { name: 'estrela', icon: estrela },
+   { name: 'b7web', icon: b7web },
+   { name: 'gasolina', icon: gasolina },
+   { name: 'moto', icon: moto },
+];
